3f05f1360c59a2a27eb8163afed9887cc05a98ca
Dawid Janczyk <45544015+DawidJanczyk@users.noreply.github.com>
2019-05-01 19:29:32 +0200

    Add files via upload

---
+++ Dessert/Desserts.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Dessert_Menu
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Desserts.xaml
+    /// </summary>
+    public partial class Desserts : Window
+    {
+        public Desserts()
+        {
+            InitializeComponent();
+        }
+
+        private new void GotFocus(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation animation = new DoubleAnimation();
+            animation.From = 1;
+            animation.To = 0;
+            animation.Duration = new Duration(TimeSpan.FromMilliseconds(1));
+            label.BeginAnimation(WidthProperty, animation);
+        }
+
+        private new void LostFocus(object sender, RoutedEventArgs e)
+        {
+            textBox.Text = String.Empty;
+            DoubleAnimation animation = new DoubleAnimation();
+            animation.From = 0;
+            animation.To = 150;
+            animation.Duration = new Duration(TimeSpan.FromMilliseconds(1));
+            label.BeginAnimation(WidthProperty, animation);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void ButtonHome_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow use = new MainWindow();
+            use.Show();
+            this.Close();
+        }
+
+        private void ButtonRecipe_Click(object sender, RoutedEventArgs e)
+        {
+            Recipe use = new Recipe();
+            use.Show();
+            this.Close();
+        }
+
+        private void ButtonHash_Click(object sender, RoutedEventArgs e)
+        {
+            Desserts use = new Desserts();
+            use.Show();
+            this.Close();
+        }
+
+        private void ButtonContact_Click(object sender, RoutedEventArgs e)
+        {
+            Contact use = new Contact();
+            use.Show();
+            this.Close();
+        }
+    }
+}
